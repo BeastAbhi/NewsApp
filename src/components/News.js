d63356f422ab9b0f9b6106036c15b0b6f9420741
BeastAbhi <abhishekkakade35@gmail.com>
2023-06-23 15:28:02 +0530

    fourth commit

--- src/components/News.js
+++ src/components/News.js
@@ -28,8 +28,7 @@ export class News extends Component {
     }
     async updateNews(){
       this.props.setProgress(10);
-      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&sortBy=publishedAt&category=${this.props.category}&apiKey=${this.state.page}  &page=${this.state.page}&pageSize=${this.props.pageSize}`;
-      this.setState({loading: true})
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       // let data = await fetch(url);
       // let parsedData = await data.json()
       // console.log(parsedData); 
